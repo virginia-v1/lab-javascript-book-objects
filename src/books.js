3e945a0c4975262bd3b7518a4c5b28fee5cff7a8
Virginia Vanstavel <virginiavanstavel@Virginias-MacBook-Pro.local>
2026-03-24 18:37:10 +0100

    lab completed

--- src/books.js
+++ src/books.js
@@ -38,27 +38,69 @@
 
 
 // Your code here:
-const booksArray = [];
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leigh Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains how a principled approach to learning separates success from failure."
+    }
+  }
+];
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(booksArray) {
+  return booksArray.map(book => book.details);
 }
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
-
+booksArray.forEach(book => {
+  delete book.details.language;
+});
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+
+function addReadingTime(booksArray) {
+  booksArray.forEach(book => {
+    const time = (book.pages * 500) / 90;
+    book.readingTime = Math.ceil(time);
+  });
+}
 
 
 
